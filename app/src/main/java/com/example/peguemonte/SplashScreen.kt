279062775316bd4cde369fa9ad4583156cd651c1
pvesouza <pvesouza@gmail.com>
2021-12-05 17:15:26 -0300

    splashscreen implemented

---
+++ app/src/main/java/com/example/peguemonte/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.example.peguemonte
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+
+class SplashScreen : AppCompatActivity(){
+
+    private val SPLASH_TIME:Long = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
+
+        Handler().postDelayed({
+            var intent:Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME)
+    }
+}
